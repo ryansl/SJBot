15ece4313e157443cfd6f7ba40374514c5b2eb7e
Ryan <rlincc@gmail.com>
2016-01-02 07:18:03 -0500

    Lower tolerance to prevent the reader from confusing whites with purples

--- configuration.py
+++ configuration.py
@@ -12,7 +12,7 @@ class Configuration:
     idle_x = 500                # X offset for idle position
     idle_y = 200                # Y offset for idle position
 
-    tolerance = 5               # RGB tolerance range for gem color detection
+    tolerance = 3               # RGB tolerance range for gem color detection
     skip = 5                    # Percentage (100 / skip %) of pixels averaged to determine gem color (higher = slower, but more accurate)
     unknown_threshold = 8       # Max number of unknown gems allowed before converting board
     
