143f2a3101b6325eaa0e69c0724d7085b85d9d93
Ryan Lin <ryan@ryanlin.com>
2017-09-19 22:27:51 -0400

    1080p updates to configuration.py

--- src/configuration.py
+++ src/configuration.py
@@ -1,24 +1,24 @@
 from definitions import *
 
 """
-Grid positions for 1440p:
-    (1740, 134) to (2508, 902)
+Grid positions for 1080p:
+    (1305, 100) to (1881, 676)
 """
 
 class Configuration:
-    screen_width = 2560         # Width of computer screen
-    screen_height = 1440        # Height of computer screen
+    screen_width = 1920         # Width of computer screen
+    screen_height = 1080        # Height of computer screen
 
-    offset_x = 1740             # X offset of grid
-    offset_y = 134              # Y offset of grid
-    grid_size = 768             # Size of grid (square)
-    gem_size = 96               # Size of gem (square)
+    offset_x = 1305             # X offset of grid
+    offset_y = 100              # Y offset of grid
+    grid_size = 576             # Size of grid (square)
+    gem_size = 72               # Size of gem (square)
     grid_length = 8             # Number of gems from top-bottom or left-right
 
-    idle_x = 1050                # X offset for idle position
-    idle_y = 800                 # Y offset for idle position
+    idle_x = 675                # X offset for idle position
+    idle_y = 400                # Y offset for idle position
 
-    tolerance = 4               # RGB tolerance range for gem color detection
+    tolerance = 3               # RGB tolerance range for gem color detection
     skip = 5                    # Percentage (100 / skip %) of pixels averaged to determine gem color (higher = slower, but more accurate)
     unknown_threshold = 48      # Max number of unknown gems allowed before converting board
     look_ahead_count = 3        # Look ahead X number of moves to find the best move
@@ -34,7 +34,7 @@ class Configuration:
     # Color mapping table -- maps skip -> ([color -> average rgb value])
     color_table = {
         5 : {
-            Color.white  : (15, 15, 14),
+            Color.white  : (13, 13, 12),
             Color.red    : (24, 10, 6),
             Color.blue   : (13, 28, 34),
             Color.purple : (15, 8, 15),
